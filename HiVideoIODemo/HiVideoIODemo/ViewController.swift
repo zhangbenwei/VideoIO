3c5462cb07775ae83020e0b531b4baa0447f8462
sageios <cnsage74@gmail.com>
2020-05-26 13:59:39 +0800

    1.init

---
+++ HiVideoIODemo/HiVideoIODemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiVideoIODemo
+//
+//  Created by goobird on 2020/5/26.
+//  Copyright © 2020 goobird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
